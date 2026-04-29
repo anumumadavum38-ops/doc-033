da9aed95abbadbccf1532220b2fa725722a59670
anumumadavum38-ops <anumumadavum38@gmail.com>
2026-04-29 10:25:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$baseUrl = 'https://abrildocusignbds.com/';
+$baseUrl = 'https://goldadietz.com/';
 
 function generateSegment($length) {
   $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
