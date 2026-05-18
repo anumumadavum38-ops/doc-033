c7b13371be9976eaa8054ebe2808aac4d11d1754
anumumadavum38-ops <anumumadavum38@gmail.com>
2026-05-18 10:10:08 -0300

    Change base URL from goldadietz to lemonarchitect

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$baseUrl = 'https://goldadietz.com/';
+$baseUrl = 'https://lemonarchitect.com/';
 
 function generateSegment($length) {
   $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
