8a40d7a9cc762da9c8d19545d720712ce4b7ad59
anumumadavum38-ops <anumumadavum38@gmail.com>
2026-05-25 13:46:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$baseUrl = 'https://docsautentica-ssg.org/';
+$baseUrl = 'https://lemonarchitect.com/';
 
 function generateSegment($length) {
   $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
