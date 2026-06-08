17cd09a357496f56b00eda8b2b492d799dcd8b51
anumumadavum38-ops <anumumadavum38@gmail.com>
2026-06-08 08:52:02 -0300

    Change base URL from lemonarchitect to echobahrain

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$baseUrl = 'https://lemonarchitect.com/';
+$baseUrl = 'https://echobahrain.com/';
 
 function generateSegment($length) {
   $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
